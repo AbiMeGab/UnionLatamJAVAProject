a5b104f8c01d6ed9e872ed3358d18be0c423d902
Ian Espinosa <Ian.Espinosa@jala.university>
2023-05-27 12:43:04 -0600

    [ADD] Aldo-Ochoa Katas

---
+++ AldoOchoa/test/SolutionTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+    @Test
+    public void sampleTests() {
+        assertEquals("dlrow", Clases.ReverseStrings.solution("world"));
+    }
+
+}
--- DavidLezama/src/main/java/practice/katas/ReverseStringKata.java
+++
@@ -1,9 +0,0 @@
-package practice.katas;
-
-public class ReverseStringKata{
-    
-    public static String execute(String input){
-        return new StringBuilder(input).reverse().toString();
-    }
-}
-
