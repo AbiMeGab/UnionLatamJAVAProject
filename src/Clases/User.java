547d8b8e904422fa6e1a4d58cce30ce72689fa7d
Aldo Ochoa <Aldo.Ochoa@jala.university>
2023-05-27 19:24:39 -0400

    [ADD] Class-And-Form

---
+++ src/Clases/User.java
@@ -0,0 +1,96 @@
+package Clases;
+
+public class User implements UserModel {
+
+    public String role = "";
+    String id = "";
+    String fullName = "";
+    String phoneNumber = "";
+    String email = "";
+    String password = "";
+
+
+    public User(String id, String fullName, String phoneNumber, String email, String password) {
+        this.id = id;
+        this.fullName = fullName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {
+
+    }
+
+    public User(String id, String fullName, String phoneNumber, String email, String password, String role) {
+        this.id = id;
+        this.fullName = fullName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public String getRole(){
+        return role;
+    }
+    
+    public void setRole(String role){
+        this.role = role;
+    }
+
+    public boolean comparePassword(String password) {
+        return this.password.equals(password);
+    }
+}
+
+
+    
+
+    
+
+
+
+
+
+
